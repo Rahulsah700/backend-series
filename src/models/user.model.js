c397214ef6a0304e69282010116f333427dad851
Rahulsah700 <rahulkumarsah7004767@gmail.com>
2024-03-03 20:44:34 +0530

    add register controller and bug

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -41,7 +41,7 @@ const userSchema = new Schema(
 
 
         },
-        watchGistory: [
+        watchHistory: [
             {
                 type: Schema.Types.ObjectId,
                 ref: "video"
